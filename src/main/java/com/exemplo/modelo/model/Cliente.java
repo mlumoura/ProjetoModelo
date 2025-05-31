1b4f420e96ee861f5710c786fee770cdbb589da0
Maria Luiza <mlumouraweb@gmail.com>
2025-05-31 20:23:31 -0300

    Criando classe Cliente

--- src/main/java/com/exemplo/modelo/model/Cliente.java
+++ src/main/java/com/exemplo/modelo/model/Cliente.java
@@ -1,4 +1,10 @@
 package com.exemplo.modelo.model;
 
 public class Cliente {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    // Construtores, Getters e Setters
 }
